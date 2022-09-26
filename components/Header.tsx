1c49e252acdfa7c53ce55ada298cb2f1b7457a17
Robbie Prokop <robbieprokop@gmail.com>
2022-09-26 22:03:16 +0400

    add fontawesome

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAmbulance, faAnchor } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 type Props = {};
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <FontAwesomeIcon
+        icon={faLinkedin}
+        style={{ fontSize: 25, color: "grey" }}
+      />
+    </header>
+  );
 };
 export default Header;
