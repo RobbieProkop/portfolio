81e6c499b1ca05450c9846f1f0d7a723ec981a3f
Robbie Prokop <robbieprokop@gmail.com>
2022-09-27 12:33:00 +0400

    style the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAmbulance, faAnchor } from "@fortawesome/free-solid-svg-icons";
+import { faHamburger, faBars } from "@fortawesome/free-solid-svg-icons";
 import { SocialIcon } from "react-social-icons";
-
 type Props = {};
 const Header = (props: Props) => {
   return (
-    <header>
-      {/* <FontAwesomeIcon
-        icon={faLinkedin}
-        style={{ fontSize: 25, color: "grey" }}
-      /> */}
-      <SocialIcon
+    <header className="px-5 flex justify-between sticky top-0 max-w-7xl mx-auto z-20">
+      {/* <SocialIcon
         url="https://www.linkedin.com/in/robbie-prokop-1a968a230/"
         fgColor="gray"
         bgColor="transparent"
         target="_blank"
-      />
+      /> */}
+      {/* <SocialIcon
+        url="https://www.github.com/robbieprokop"
+        fgColor="gray"
+        bgColor="transparent"
+        target="_blank"
+      /> */}
       <h2>DhammaDevs</h2>
+      <div className="flex flex-flex-row items-center cursor-pointer">
+        {/* <FontAwesomeIcon
+          icon={faBars}
+          style={{ fontSize: 25, color: "grey" }}
+          className="sm:inline-flex md:hidden "
+        /> */}
+        <h3 className="hidden md:inline-flex text-sm text-gray-400 pr-3">
+          about
+        </h3>
+        <h3 className="hidden md:inline-flex text-sm text-gray-400 pr-3">
+          projects
+        </h3>
+        <h3 className="hidden md:inline-flex text-sm text-gray-400">contact</h3>
+      </div>
     </header>
   );
 };
