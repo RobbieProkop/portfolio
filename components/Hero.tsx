cdf35500c453ad9ecfd67f57bad06994d7c9e855
Robbie Prokop <robbieprokop@gmail.com>
2022-09-27 17:22:05 +0400

    workingo n h1 animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,27 +4,50 @@ type Props = {};
 const Hero = (props: Props) => {
   return (
     <motion.div
-      initial={{
-        opacity: 0,
-        scale: 0,
-      }}
       animate={{
-        opacity: 1,
-        scale: 1,
-        rotate: 360,
-        // rotateY: 360,
+        scale: [0, 1, 2, 2, 1, 1],
+        rotate: [0, 0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "0%", "50%", "50%", "0%"],
       }}
       transition={{
-        delay: 2,
-        duration: 4.5,
-        type: "spring",
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        // repeat: Infinity,
+        // repeatDelay: 1,
       }}
-      className="h-screen flex flex-col justify-center items-center"
+      className="box flex items-center justify-center w-full"
     >
-      <div className="line"></div>
-      <h1 className="">Hi I'm Robbie</h1>
-      <div className="line"></div>
+      <motion.div className="h-screen flex flex-col justify-center items-center">
+        <div className="line"></div>
+        <h1 className="">Hi I'm Robbie</h1>
+        <div className="line"></div>
+      </motion.div>
     </motion.div>
   );
 };
+// <motion.div
+//   initial={{
+//     opacity: 0,
+//     scale: 0,
+//     x: 0,
+//   }}
+//   animate={{
+//     opacity: 1,
+//     scale: 1,
+//     // rotate: 360,
+//     x: 500,
+//     // rotateY: 360,
+//   }}
+//   transition={{
+//     delay: 2,
+//     duration: 4.5,
+//     type: "spring",
+//   }}
+//   className="h-screen flex flex-col justify-center items-center"
+// >
+//   <div className="line"></div>
+//   <h1 className="">Hi I'm Robbie</h1>
+//   <div className="line"></div>
+// </motion.div>
 export default Hero;
