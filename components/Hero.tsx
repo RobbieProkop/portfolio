ab09b579108d321e0865b9873d27ec185fd98b7a
Robbie Prokop <robbieprokop@gmail.com>
2022-09-27 20:38:49 +0400

    change marquee text to scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,67 @@
-import { motion } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+import { useRef } from "react";
 
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+const ParallaxText = ({ children, baseVelocity = 100 }: ParallaxProps) => {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  const prevT = useRef<number>(0);
+  useAnimationFrame((t) => {
+    if (!prevT.current) prevT.current = t;
+
+    const timeDelta = t - prevT.current;
+    let moveBy = directionFactor.current * baseVelocity * (timeDelta / 1000);
+
+    //used to change the scroll direction
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+
+    prevT.current = t;
+  });
+
+  return (
+    <div className="parallax">
+      <motion.div className="scroller" style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
+  );
+};
 type Props = {};
 const Hero = (props: Props) => {
   return (
@@ -40,7 +102,12 @@ const Hero = (props: Props) => {
         <h1 className="">Hi, I'm Robbie</h1>
 
         <div className="line"></div>
-        <div className="marquee mx-auto w-6xl">
+
+        <ParallaxText baseVelocity={5}>
+          Frontend-Backend-FullStack-
+        </ParallaxText>
+
+        {/* <div className="marquee mx-auto w-6xl">
           <div className="marquee-content">
             <ul className="list">
               <h2>Frontend</h2>
@@ -58,7 +125,7 @@ const Hero = (props: Props) => {
               <h2>Full-Stack</h2>
             </ul>
           </div>
-        </div>
+        </div> */}
         <div className="line"></div>
       </motion.div>
     </>
