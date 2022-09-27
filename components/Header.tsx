3e77b2c40c5ea7d4099e74d8b0e2f962fa225743
Robbie Prokop <robbieprokop@gmail.com>
2022-09-27 12:02:45 +0400

    add social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAmbulance, faAnchor } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 const Header = (props: Props) => {
   return (
     <header>
-      <FontAwesomeIcon
+      {/* <FontAwesomeIcon
         icon={faLinkedin}
         style={{ fontSize: 25, color: "grey" }}
+      /> */}
+      <SocialIcon
+        url="https://www.linkedin.com/in/robbie-prokop-1a968a230/"
+        fgColor="gray"
+        bgColor="transparent"
+        target="_blank"
       />
+      <h2>DhammaDevs</h2>
     </header>
   );
 };
