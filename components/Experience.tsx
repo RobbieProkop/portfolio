4cbfff9f8a32641efde17ff3df57fe6509ab1d29
Robbie Prokop <robbieprokop@gmail.com>
2022-09-29 14:03:56 +0400

    start styling experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,8 +1,10 @@
 type Props = {};
 const Experience = (props: Props) => {
   return (
-    <div className="h-screen">
-      <div>experience</div>
+    <div className="h-screen flex relative  overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly items-center mx-auto">
+      <h3 className="absolute top-0 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
     </div>
   );
 };
