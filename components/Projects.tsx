bc65d2660f0b8dd8de071436b969607145bd5fe0
Robbie Prokop <robbieprokop@gmail.com>
2022-09-30 13:19:09 +0400

    motion image

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,12 @@ const Projects = (props: Props) => {
   // dummy array
   const projects = ["NLIF", "ZiziKarma", "Paw-Patrol", "API-Mania"];
   return (
-    <motion.div className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly items-center mx-auto z-0">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly items-center mx-auto z-0"
+    >
       <h3 className="absolute top-10 uppercase tracking-[20px] text-gray-500 text-2xl mb-5">
         Projects
       </h3>
@@ -15,9 +20,21 @@ const Projects = (props: Props) => {
       <div className="relative w-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project) => (
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-            <div className="h-[350px] w-[350px]">
+            <motion.div
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{ duration: 1.2 }}
+              viewport={{ once: true }}
+              className="h-[350px] w-[350px]"
+            >
               <Image src={monk} objectFit="contain" />
-            </div>
+            </motion.div>
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
                 <span className="underline decoration-green-600/50">
