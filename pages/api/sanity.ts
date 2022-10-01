7b992f463b97618b8fdab53ac88f73a9b499cdee
Robbie Prokop <robbieprokop@gmail.com>
2022-10-01 15:41:14 +0400

    install next-sanity. create sanity.ts config

---
+++ pages/api/sanity.ts
@@ -0,0 +1,9 @@
+import { createClient } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url";
+
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-03-25",
+  useCdn: process.env.NODE_ENV === "production",
+};
