c84300d5ff5269f6b2f62c01bd25d05fa1c78958
Robbie Prokop <robbieprokop@gmail.com>
2022-10-01 17:15:06 +0400

    begin type definitions

---
+++ pages/api/getSocials.ts
@@ -0,0 +1,22 @@
+// Next JS API route support: https://nextjsx.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { Social } from "../../typings";
+import { sanityClient } from "./sanity";
+// import {PageInfo, Social} from "../../typings"
+
+const query = groq`
+  *[_type == "social"]
+`;
+
+type Data = {
+  socials: Social[];
+};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const socials: Social[] = await sanityClient.fetch(query);
+  res.status(200).json({ socials });
+}
---
+++ typings.d.ts
@@ -0,0 +1,63 @@
+interface SanityBody {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+}
+
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+export interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  backgroundInformation: string;
+  email: string;
+  role: string;
+  name: string;
+  phoneNumber: string;
+  profilePic: Image;
+}
+
+export interface Technology extends SanityBody {
+  _type: "skill";
+  image: Image;
+  title: string;
+}
+
+export interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  title: string;
+}
+
+export interface Experience extends SanityBody {
+  _type: "experience";
+  company: string;
+  companyImage: Image;
+  dateStarted: date;
+  dateEnded: date;
+  isCurrentlyWorkingHere: boolean;
+  jobTitle: string;
+  ponts: string[];
+  technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+  title: string;
+  _type: "project";
+  image: Image;
+  linkToBuild: string;
+  summary: string;
+  technologies: Technology[];
+}
+
+export interface Social extends SanityBody {
+  _type: "social";
+  title: string;
+  url: string;
+}
