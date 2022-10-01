cd160955986d9901c13eefb74591c43a5fdf4e0c
Robbie Prokop <robbieprokop@gmail.com>
2022-10-01 15:44:19 +0400

    add functions to sanity.ts

--- pages/api/sanity.ts
+++ pages/api/sanity.ts
@@ -1,9 +1,15 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
-const config = {
+export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
+
+//Set up the client for fetching data in getProps page function
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) =>
+  createImageUrlBuilder(config).image(source);
