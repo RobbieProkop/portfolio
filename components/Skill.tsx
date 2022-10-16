6cc55035c37874056ec65db3358e2317ac685daf
Robbie Prokop <robbieprokop@gmail.com>
2022-10-16 13:29:24 +0400

    add mobile check for motion

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,22 +19,43 @@ const configuredSanityClient = sanityClient({
 
 const Skill = ({ skill, directionLeft }: Props) => {
   const imageProps = useNextSanityImage(configuredSanityClient, skill.image);
+  const isMobile = window.innerWidth < 768;
+
+  console.log(window.innerWidth);
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.div
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0,
-        }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }} // used only if want to happen once
-        transition={{ duration: 1 }}
-        // group-hover:animate-bounce
-        className="h-24 w-24 flex items-center justify-center border-gray-500 xl:h-32 xl:w-32 filter group-hover:grayscale group-hover:animate-pulse
+      {isMobile && (
+        <motion.div
+          // initial={{
+          //   x: directionLeft ? -200 : 200,
+          //   opacity: 0,
+          // }}
+          // whileInView={{ opacity: 1, x: 0 }}
+          // viewport={{ once: true }} // used only if want to happen once
+          // transition={{ duration: 1 }}
+          // // group-hover:animate-bounce
+          className="h-24 w-24 flex items-center justify-center border-gray-500 xl:h-32 xl:w-32 filter group-hover:grayscale group-hover:animate-pulse
         transition duration-300 ease-in-out"
-      >
-        <Image {...imageProps} alt="" />
-      </motion.div>
+        >
+          <Image {...imageProps} alt="" />
+        </motion.div>
+      )}
+      {!isMobile && (
+        <motion.div
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0,
+          }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }} // used only if want to happen once
+          transition={{ duration: 1 }}
+          // // group-hover:animate-bounce
+          className="h-24 w-24 flex items-center justify-center border-gray-500 xl:h-32 xl:w-32 filter group-hover:grayscale group-hover:animate-pulse
+        transition duration-300 ease-in-out"
+        >
+          <Image {...imageProps} alt="" />
+        </motion.div>
+      )}
     </div>
   );
 };
