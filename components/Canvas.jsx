1390465e505fd755ce1abe2a6e702e911c1c874f
Robbie Prokop <robbieprokop@gmail.com>
2022-11-27 15:42:06 +0400

    begin rain effect

---
+++ components/Canvas.jsx
@@ -0,0 +1,9 @@
+const Canvas = () => {
+  const canvas = document.getElementById("canvas1");
+  return (
+    <div id="canvas1" className="canvas">
+      Canvas
+    </div>
+  );
+};
+export default Canvas;
